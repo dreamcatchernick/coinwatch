3fbbc18bea9b83d2c7f08ad22f05773bfa8fcdda
dreamcatchernick <bingu@quarkfinance.com>
2018-07-23 15:11:36 +0800

    add

--- app.js
+++ app.js
@@ -2,50 +2,40 @@ const axios = require('axios');
 
 const bittrex = 'https://bittrex.com/api/v1.1/public/getorderbook?market=USDT-ADA&type=both';
 const binance = 'https://api.binance.com/api/v1/depth?symbol=ADAUSDT&limit=5';
-function run() {
-    getPrice(bittrex).then(data => {
-        console.log('===========Bittrex==============');
-        data = data.result;
-        const buyPrice = data.buy[0].Rate;
-        const buyAmount = data.buy[0].Quantity;
+async function run() {
+    const bittrexResponse = await getPrice(bittrex);
 
-        const sellPrice = data.sell[0].Rate;
-        const sellAmount = data.sell[0].Quantity;
+    const bittrexData = bittrexResponse.result;
+    const bittrexBuyPrice = bittrexData.buy[0].Rate;
+    const bittrexBuyAmount = bittrexData.buy[0].Quantity;
 
-        const buyMessage = `buy price:${buyPrice} , amount:${buyAmount}`;
-        const sellMessage = `sell price:${sellPrice} , amount:${sellAmount}`;
-        console.log(buyMessage);
-        console.log(sellMessage);
-        console.log('================================');
-    });
+    const bittrexSellPrice = bittrexData.sell[0].Rate;
+    const bittrexSellAmount = bittrexData.sell[0].Quantity;
 
-    getPrice(binance).then(data=> {
-        console.log('===========Binance==============');
-        const buyItem = data.bids[0];
-        const buyPrice = buyItem[0];
-        const buyAmount = buyItem[1];
+    const bittrexBuyMessage = `Bittrex buy price:${bittrexBuyPrice} , amount:${bittrexBuyAmount}`;
+    const bittrexSellMessage = `Bittrex sell price:${bittrexSellPrice} , amount:${bittrexSellAmount}`;
 
-        const sellItem = data.asks[0];
-        const sellPrice = sellItem[0];
-        const sellAmount = sellItem[1];
 
-        const buyMessage = `buy price:${buyPrice} , amount:${buyAmount}`;
-        const sellMessage = `sell price:${sellPrice} , amount:${sellAmount}`;
-        console.log(buyMessage);
-        console.log(sellMessage);
-        console.log('================================');
-    })
+    const binanceResponse = await getPrice(binance);
+    const binanceBuyPrice = binanceResponse.bids[0][0];
+    const binanceBuyAmount = binanceResponse.bids[0][1];
+
+    const binanceSellPrice = binanceResponse.asks[0][0];
+    const binanceSellAmount = binanceResponse.asks[0][1];
+
+    const binanceBuyMessage = `Binance buy price:${binanceBuyPrice} , amount:${binanceBuyAmount}`;
+    const binanceSellMessage = `Binance sell price:${binanceSellPrice} , amount:${binanceSellAmount}`;
+
+    console.log(bittrexBuyMessage);
+    console.log(binanceSellMessage);
+    console.log('================================');
+    console.log(bittrexSellMessage);
+    console.log(binanceBuyMessage);
 }
 
-function getPrice(url) {
-    return axios.get(url)
-                .then(response => {
-                    // console.log(response);
-                    return response.data;
-                })
-                .catch(error => {
-                    console.log(error.message);
-                })
+async function getPrice(url) {
+    const response = await axios.get(url);
+    return response.data;
 }
 
 setInterval(run, 4000);
